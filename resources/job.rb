dde5a25e47e2e9cb56d92263957a7aba775ba813
Peter Green <peter.green@dachisgroup.com>
2013-08-27 16:38:08 +0100

    Updated the jobs LWRP to accept and pass through the excludes config option

--- resources/job.rb
+++ resources/job.rb
@@ -3,6 +3,7 @@ actions :create
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :label, :kind_of => String
 attribute :path, :kind_of => String
+attribute :exclude, :kind_of => Array
 attribute :monitor, :kind_of => String
 attribute :recursive, :kind_of => [TrueClass, FalseClass]
 attribute :command, :kind_of => String
