70ee1d2876b29372449e77ea74a3397879feead4
Peter Green <peter.green@dachisgroup.com>
2013-08-28 10:49:51 +0100

    Added an attribute for selecting a particular branch/commit

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['watcher']['repo_url'] = "https://github.com/greggoryhz/Watcher.git"
+default['watcher']['branch'] = "master"
 default['watcher']['install_path'] = "/usr/local/watcher"
 default['watcher']['pid_file'] = "/var/run/watcher.pid"
 default['watcher']['log_file'] = "/var/log/watcher.log"
